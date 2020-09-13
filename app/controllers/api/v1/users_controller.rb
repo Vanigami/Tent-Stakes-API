fe147b4093f09cc85ebcdffe380a5b05a2f14615
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-09-13 10:37:56 -0400

    add cors middleware

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,17 @@
 class Api::V1::UsersController < ApplicationController
+    # GET /users
+
+    # GET /user/:id
+
+    #POST /users
+
+    #PATCH /users/:id
+
+    #DELETE /users/:id
+
+
+
+
+
+
 end
