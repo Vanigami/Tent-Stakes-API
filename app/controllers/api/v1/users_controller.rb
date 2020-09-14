7d8c12db59a4d50d4039818957dc7f27058b6eb3
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-09-14 18:32:54 -0400

    add create method and error handling

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,7 +11,14 @@ class Api::V1::UsersController < ApplicationController
         render json: @user
     end
     #POST /users
-
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user
+        else
+            render error: { error 'Unable to create User'}, status: 400
+        end
+    end
     #PATCH /users/:id
 
     #DELETE /users/:id
