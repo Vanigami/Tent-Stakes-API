1dedc7d4a6d1ba0ff4f9c0b415d97e15aed994d8
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-09-14 18:35:39 -0400

    create update method with error handling

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -16,11 +16,19 @@ class Api::V1::UsersController < ApplicationController
         if @user.save
             render json: @user
         else
-            render error: { error 'Unable to create User'}, status: 400
+            render error: { error 'Unable to create User :('}, status: 400
         end
     end
     #PATCH /users/:id
-
+    def update
+        @user = User.find(params[:id])
+        if @user 
+            @user.update(user_params)
+            render json: { message: 'User successfully updated!'}, status: 200
+        else 
+            render json: { message: 'Unable to update User :('}, status: 400
+        end
+    end
     #DELETE /users/:id
 
 
