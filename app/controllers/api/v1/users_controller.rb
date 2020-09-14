5627206cb0bef25c808ae3047125d7b1cc91d958
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-09-14 18:31:04 -0400

    fix snytax error in user model and add index and show to user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,15 @@
 class Api::V1::UsersController < ApplicationController
     # GET /users
+    def index
+        @users = User.all 
+        render json: @users
+    end
 
     # GET /user/:id
-
+    def show 
+        @user = User.find(params[:id])
+        render json: @user
+    end
     #POST /users
 
     #PATCH /users/:id
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ApplicationRecord
-    has_many: checklists
+    has_many :checklists
 end
