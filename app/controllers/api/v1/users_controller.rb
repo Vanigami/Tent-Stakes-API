3d82c3d566db7daf80bbbdf086fc6496e4addd18
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-09-14 18:41:45 -0400

    add user params private method'

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -40,6 +40,13 @@ class Api::V1::UsersController < ApplicationController
         end
     end
 
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+end
+
 
 
 
