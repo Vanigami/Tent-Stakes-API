e25dacecceb615865b910a0f4e415f9a05a34714
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-09-17 20:03:37 -0400

    add nested route for checklist items

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do 
       resources :users do 
-        resources :checklists
+        resources :checklists do
+        resources :items
+        end
       end
     end
   end
