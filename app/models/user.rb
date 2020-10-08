ef7a07d7967a17805dfb0607956dbdbdfeed09c3
Vanigami <39018244+Vanigami@users.noreply.github.com>
2020-10-08 17:48:08 -0400

    rename

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :checklists
-    validates_presence_of :name, :password 
-    validates_uniqueness_of :name
+    validates_presence_of :username, :password 
+    validates_uniqueness_of :username"
+
 end
